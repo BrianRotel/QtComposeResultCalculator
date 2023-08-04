6fe5953f499ce245be65b39e59f2f6f802dafe5c
路遥 景 <809030108@qq.com>
2023-08-05 07:38:41 +0800

    添加opencv的图像边缘检测--这个版本修改了vs的属性,添加了opencv的包含路径和lib库路径,使用了绝对路径.项目移植的时候需要注意.

---
+++ QtComposeResultCalculator/QtOpenCV_Worker.cpp
@@ -0,0 +1,53 @@
+﻿#include "stdafx.h"
+#include "QtOpenCV_Worker.h"
+#include<QDebug>
+#include<QThread>
+
+QtOpenCV_Worker::QtOpenCV_Worker(QObject *parent)
+	: QObject(parent)
+{}
+
+QtOpenCV_Worker::~QtOpenCV_Worker()
+{}
+
+//flag是一个任务类型标识，0的时候做Canny边缘检测，1的时候做拉普拉斯算子
+void QtOpenCV_Worker::task(cv::Mat* src, cv::Mat* dst, uchar flag)
+{
+    if (src->data == NULL)
+        return;
+    if (flag == 0) //Canny边缘检测
+    {
+        cv::Canny(*src, *dst, 20, 40, 3); //3是直径，应在3~7范围内，否则会报错
+        //        qDebug()<<QThread::currentThreadId();
+    }
+    if (flag == 1) //拉普拉斯算子，梯度的散度
+    {
+        this->laplaceGrad(*src, *dst);
+    }
+    emit threadId((int*)QThread::currentThreadId()); //当前线程id返回
+}
+
+//laplace任务
+void QtOpenCV_Worker::laplaceGrad(cv::Mat& src, cv::Mat& dst)
+{
+
+    if (src.channels() != 1)
+        return;
+
+    int rows = src.rows;
+    int cols = src.cols;
+
+    dst = cv::Mat::zeros(rows, cols, CV_32FC1);
+
+    for (int i = 1; i < rows - 1; i++)
+    {
+        for (int j = 1; j < cols - 1; j++)
+        {
+            //            [1,1,1;1,-8,1;1,1,1]
+            dst.ptr<float>(i)[j] = 1.0 * src.ptr<uchar>(i - 1)[j - 1] + 1.0 * src.ptr<uchar>(i - 1)[j] + 1.0 * src.ptr<uchar>(i - 1)[j + 1]
+                + 1.0 * src.ptr<uchar>(i)[j - 1] - 8.0 * src.ptr<uchar>(i)[j] + 1.0 * src.ptr<uchar>(i)[j + 1]
+                + 1.0 * src.ptr<uchar>(i + 1)[j - 1] + 1.0 * src.ptr<uchar>(i + 1)[j] + 1.0 * src.ptr<uchar>(i + 1)[j + 1];
+        }
+    }
+    cv::convertScaleAbs(dst, dst, 1, 0); // 从CV_32FC1转换到CV_8UC1
+}
---
+++ QtComposeResultCalculator/QtOpenCV_Worker.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <QObject>
+#include<opencv2/opencv.hpp>
+#include<opencv2/imgproc/types_c.h>
+using namespace cv;
+
+class QtOpenCV_Worker  : public QObject
+{
+	Q_OBJECT
+
+public:
+	QtOpenCV_Worker(QObject *parent);
+	~QtOpenCV_Worker();
+    void laplaceGrad(cv::Mat& src, cv::Mat& dst);
+signals:
+    void threadId(int*); //这个信号用来返回线程id标识
+public slots:
+    //槽函数，子线程任务在这个函数中处理
+    void task(cv::Mat* src, cv::Mat* dst, uchar flag);
+};
+
+
