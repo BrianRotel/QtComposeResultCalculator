9ba46696a875b3a8deb62656801e1e4b21350d81
路遥 景 <809030108@qq.com>
2023-08-16 05:16:01 +0800

    测试后台点击,失败了

--- QtComposeResultCalculator/QtGrabWindow.cpp
+++ QtComposeResultCalculator/QtGrabWindow.cpp
@@ -2,9 +2,14 @@
 #include "QtGrabWindow.h"
 #include <QElapsedTimer>
 #include <QTest>
+#include <Winuser.h>
+#include <commctrl.h>
+#include <Winbase.h>
 
 #include "public.hpp"
 using namespace cv;
+void RemoteHeaderItem(HWND hWnd);
+void RemoteClick(HWND hWnd, POINT p,UINT Msg);
 
 //Windows系统中使用DXGI截取桌面图像 此代码中DXGI获取图像来源于 https://www.cnblogs.com/TechNomad/p/17428347.html 
 //Windows系统中使用GDI截取桌面图像
@@ -612,17 +617,18 @@ void QtGrabWindow::slotTimer()
 	{
 		return;
 	}
-	::SetForegroundWindow(hwnd);
+	//::SetForegroundWindow(hwnd);
 
 	QImage showImg = findPicture(pix.toImage(), QImage(":/QtComposeResultCalculator/Resource/test2.png"));
 
 	QWindow* windowLogin = QWindow::fromWinId((WId)hwnd);
 	windowLogin->setFramePosition(QPoint(0,31));
 	//QSize size = windowLogin->size();
-	//POINT pos = { 0,0 };
+	POINT pos = { clickP.x(),clickP.y() };
 	//GetCursorPos(&pos);
 	//qDebug() << "x" << pos.x << "y" << pos.y;
 	clickP.setY(clickP.y()+31);
+
 	qDebug() << clickP;
 
 
@@ -644,15 +650,29 @@ void QtGrabWindow::slotTimer()
 		mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP | MOUSEEVENTF_MOVE, clickP.x() * bx, clickP.y() * by, 0, 0);
 		//::ScreenToClient(hwnd, &pos); // 把pos转成相对于窗口客户区的坐标
 		//qDebug() << "x1" << pos.x << "y1" << pos.y;
-		//::SetFocus(hwnd);
-		//bool isS = ::SendMessage(hwnd, WM_LBUTTONDOWN, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
-		//bool isS1 = ::SendMessage(hwnd, WM_LBUTTONUP, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
+		::SetFocus(hwnd);
+		//RemoteHeaderItem(hwnd);
+		//传递过去的消息应该是不正确的
+		//RemoteClick(hwnd, pos, WM_ACTIVATE);
+		//RemoteClick(hwnd, pos, WM_SETFOCUS);
+		//RemoteClick(hwnd, pos, WM_MOUSEACTIVATE);
+		//RemoteClick(hwnd, pos, WM_MOUSEMOVE);
+		//RemoteClick(hwnd, pos, WM_INPUT);
+		//RemoteClick(hwnd, pos, WM_LBUTTONDOWN);
+		//RemoteClick(hwnd, pos, WM_INPUT);
+		//RemoteClick(hwnd, pos, WM_LBUTTONUP);
+		//RemoteClick(hwnd, pos, WM_MOUSEMOVE);
+
+		//bool isS0 = ::SendMessage(hwnd, WM_INPUT, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
+		//bool isS1 = ::SendMessage(hwnd, WM_LBUTTONDOWN, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
+		//bool isS2 = ::SendMessage(hwnd, WM_INPUT, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
+		//bool isS3 = ::SendMessage(hwnd, WM_LBUTTONUP, 0, MAKELPARAM((long)clickP.x(), (long)clickP.y()));
 		//bool isS = ::SendMessage(hwnd, WM_LBUTTONDOWN, 0, MAKELPARAM(pos.x, pos.y));
 		//bool isS1 = ::SendMessage(hwnd, WM_LBUTTONUP, 0, MAKELPARAM(pos.x, pos.y));
 		//qDebug() << "isS" << isS;
 		//qDebug() << "isS1" << isS1;
 		//qDebug() << "error:" << GetLastError();
-		QTimer::singleShot(2000, this, &QtGrabWindow::clickOther);
+		QTimer::singleShot(1000, this, &QtGrabWindow::clickOther);
 	}
 	qDebug() << "R" << QString::number(pixcolor.red());//255 127
 	//qDebug() << "G" << QString::number(pixcolor.green());//0 15
@@ -671,6 +691,7 @@ void QtGrabWindow::slotTimer()
 	QImage img2 = pix.copy(mX, mY, mW, mH).toImage();
 	resize(img2.size());
 #endif
+#ifdef ISPAINT
 	//绘制截屏
 	QPalette palette = this->palette();
 	QBrush brush = palette.brush(QPalette::Window);
@@ -678,8 +699,102 @@ void QtGrabWindow::slotTimer()
 	palette.setBrush(QPalette::Window, brush);
 	this->setPalette(palette);
 	resize(pix.size());
-
+#endif // DEBUG
 	qDebug() <<"timeElapsed"<< e.elapsed();
+}
+void RemoteClick(HWND hWnd, POINT pP, UINT Msg)
+{
+	// Get hProcess
+	DWORD pid;
+	GetWindowThreadProcessId(hWnd, &pid);
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	//PrefetchVirtualMemory
+	POINT* remote_ptr = (POINT*)VirtualAllocEx(hProcess, 0, sizeof(POINT), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (!remote_ptr)
+	{
+		//::MessageBox(NULL, "无法分配内存!", "错误!", NULL);
+		qDebug() << "无法分配内存!";
+		QMessageBox::warning(nullptr,"错误!","无法分配内存!");
+		CloseHandle(hProcess);
+		return;
+	}
+	//
+	MSG msg;
+	msg.hwnd = hWnd;
+	msg.message = Msg;
+	msg.wParam = 0; 
+	msg.lParam = MAKELPARAM(pP.x, pP.y);
+	msg.time = 50; 
+	msg.pt = pP;
+
+	//
+	{
+		WriteProcessMemory(hProcess, remote_ptr, &msg, sizeof(MSG), 0);
+		::SendMessage(hWnd, Msg, 0, (LPARAM)remote_ptr);
+		//		afxDump << ItemBuf << "\n";
+	}
+	//释放内存
+	CloseHandle(hProcess);
+	VirtualFreeEx(hProcess, remote_ptr, 0, MEM_RELEASE);
+}
+
+void RemoteHeaderItem(HWND hWnd)
+{
+	// GetItemCount
+	int nHeaderCount = ::SendMessage(hWnd, HDM_GETITEMCOUNT, 0, 0);
+	// Get hProcess
+	DWORD pid;
+	GetWindowThreadProcessId(hWnd, &pid);
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	//PrefetchVirtualMemory
+	HDITEM* pHeaderItem = (HDITEM*)VirtualAllocEx(hProcess, NULL, sizeof(HDITEM), MEM_COMMIT, PAGE_READWRITE);
+	char* pItem = (char*)VirtualAllocEx(hProcess, NULL, 80, MEM_COMMIT, PAGE_READWRITE);
+	if ((!pHeaderItem) || (!pItem))
+	{
+		//::MessageBox(NULL, "无法分配内存!", "错误!", NULL);
+		CloseHandle(hProcess);
+		return;
+	}
+	//
+	HDITEM HeaderItem;
+	HeaderItem.cchTextMax = 80;
+	HeaderItem.mask = HDI_TEXT;
+	HeaderItem.pszText = reinterpret_cast<LPWSTR>(pItem);
+	//
+	char ItemBuf[80];
+	for (int i = 0; i < nHeaderCount; i++)
+	{
+		HeaderItem.iOrder = i;
+		WriteProcessMemory(hProcess, pHeaderItem, &HeaderItem, sizeof(HDITEM), NULL);
+		::SendMessage(hWnd, HDM_GETITEM, i, (LPARAM)pHeaderItem);
+		ReadProcessMemory(hProcess, pItem, ItemBuf, 80, NULL);
+		//		afxDump << ItemBuf << "\n";
+	}
+	//释放内存
+	CloseHandle(hProcess);
+	VirtualFreeEx(hProcess, pHeaderItem, 0, MEM_RELEASE);
+	VirtualFreeEx(hProcess, pItem, 0, MEM_RELEASE);
+}
+BOOL RegisitKeyBord(HWND hwnd)
+{
+	if (NULL == hwnd)
+		return false;
+
+	RAWINPUTDEVICE rid;  //设备信息
+	rid.usUsagePage = 0x01;
+	rid.usUsage = 0x02; //键盘   rid.usUsagePage = 0x01; rid.usUsage = 0x02; 为鼠标
+	rid.dwFlags = RIDEV_INPUTSINK;
+	rid.hwndTarget = hwnd;
+
+	return RegisterRawInputDevices(&rid, 1, sizeof(RAWINPUTDEVICE));
+
+#ifdef _THIS_OTHER_CODE
+	HGLOBAL hRaw = ::GlobalAlloc(GHND, sizeof(RAWINPUT));
+	RAWINPUT* pRaw = reinterpret_cast<RAWINPUT*>(::GlobalLock(hRaw));
+	// initialize the structure using pRaw
+	::GlobalUnlock(hRaw);
+	// use hRaw as the LPARAM
+#endif
 }
 //将下面的代码设置到主函数,调用函数进行bitmap图片的保存
 #ifdef DXGI
@@ -734,3 +849,296 @@ void QtGrabWindow::mouseMoveEvent(QMouseEvent* event)
 	qDebug() << "B" << QString::number(pixcolor.blue());
 #endif
 }
+
+#if 0
+//关于raw input 请查看msdn https://msdn.microsoft.com/en-us/library/windows/desktop/ms645536%28v=vs.85%29.aspx//效率 比 键盘钩子高太多 ，但是BCB 中无法识别 RAWINPUTDEVICE  结构 和RAW相关的函数// rawinput4.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "rawinput4.h"
+#include <set>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+TCHAR szTitle[MAX_LOADSTRING];                    // 标题栏文本
+TCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+set<DWORD> keys;
+
+//避免按住不放不停的响应keydown
+bool IsInKeys(DWORD key)
+{
+	set<DWORD>::iterator itor =
+		find(keys.begin(), keys.end(), key);
+
+	bool result = false;
+	if (itor == keys.end())
+	{
+		keys.insert(key);
+		result = false;
+	}
+	else
+	{
+		result = true;
+	}
+	return result;
+}
+//松开的时候，将key从set中移除
+void erasekey(DWORD key)
+{
+	keys.erase(key);
+}
+
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+
+void RegKeyboardRawInput(HWND hwnd)
+{
+	RAWINPUTDEVICE Rid[1];
+	Rid[0].usUsagePage = 0x01;
+	Rid[0].usUsage = 0x06;
+	Rid[0].dwFlags = RIDEV_INPUTSINK;//RIDEV_NOLEGACY;   // adds HID keyboard and also ignores legacy keyboard messages
+	Rid[0].hwndTarget = hwnd;
+
+	if (RegisterRawInputDevices(Rid, 1, sizeof(Rid[0])) == FALSE) {
+		MessageBox(NULL, L"注册raw input 失败！", L"注册 raw input", MB_OK + MB_ICONINFORMATION);
+	}
+
+}
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 初始化全局字符串
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_RAWINPUT4, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 执行应用程序初始化:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_RAWINPUT4));
+
+	// 主消息循环:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目的: 注册窗口类。
+//
+//  注释:
+//
+//    仅当希望
+//    此代码与添加到 Windows 95 中的“RegisterClassEx”
+//    函数之前的 Win32 系统兼容时，才需要此函数及其用法。调用此函数十分重要，
+//    这样应用程序就可以获得关联的
+//    “格式正确的”小图标。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_RAWINPUT4));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_RAWINPUT4);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目的: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+
+	hInst = hInstance; // 将实例句柄存储在全局变量中
+
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	RegKeyboardRawInput(hWnd);
+
+	return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的: 处理主窗口的消息。
+//
+//  WM_COMMAND    - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY    - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 分析菜单选择:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 在此添加任意绘图代码...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_INPUT:
+	{
+		UINT dwSize;
+
+		GetRawInputData((HRAWINPUT)lParam, RID_INPUT, NULL, &dwSize,
+			sizeof(RAWINPUTHEADER));
+		LPBYTE lpb = new BYTE[dwSize];
+		if (lpb == NULL)
+		{
+			return 0;
+		}
+
+		if (GetRawInputData((HRAWINPUT)lParam, RID_INPUT, lpb, &dwSize,
+			sizeof(RAWINPUTHEADER)) != dwSize)
+			OutputDebugString(TEXT("GetRawInputData doesn't return correct size !\n"));
+
+		RAWINPUT* raw = (RAWINPUT*)lpb;
+
+		if (raw->header.dwType == RIM_TYPEKEYBOARD)
+		{
+
+
+			if (raw->data.keyboard.Message == WM_KEYDOWN)
+			{
+				if (!IsInKeys(raw->data.keyboard.VKey))
+				{
+					char keytext[10] = { 0 };
+					BYTE state[256] = { 0 };
+
+					//通过虚拟键盘码得到名字
+					ToAscii(raw->data.keyboard.VKey, raw->data.keyboard.MakeCode, state, (LPWORD)keytext, 0);
+
+					char buf[10] = { 0 };
+
+					string s = "键盘按下 虚键码:" + string(keytext);
+
+					MessageBoxA(NULL, s.c_str(), "键盘", MB_OK);
+				}
+
+			}
+
+			if (raw->data.keyboard.Message == WM_KEYUP)
+			{
+				erasekey(raw->data.keyboard.VKey);
+			}
+		}
+
+
+		delete[] lpb;
+		return 0;
+	}
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+#endif
